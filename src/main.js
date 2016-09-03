cd9b472b555f62c8ecaeeb12dd1fa54323887279
Astlvk <astlvk@vip.qq.com>
2016-09-03 10:33:34 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import mainView from './component/mainView'
-const app = new Vue({
-  el: '#app',
+new Vue({
   data: {
     message: 'Hello Vue2.0',
   },
   components: {
     mainView,
   }
-});
+}).$mount('#app');
